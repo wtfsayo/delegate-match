35b825246d9c62dc3da5d49df0eae8b9a5f1a384
memosys <82053242+wtfsayo@users.noreply.github.com>
2024-07-21 12:19:19 +0530

    getting state well

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,99 +1,126 @@
 /** @jsxImportSource frog/jsx */
 
-import { Button, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
-import { neynar } from 'frog/hubs'
-import { handle } from 'frog/next'
-import { serveStatic } from 'frog/serve-static'
+import { Button, Frog, TextInput } from "frog";
+import { devtools } from "frog/dev";
+import { neynar } from "frog/hubs";
+import { handle } from "frog/next";
+import { serveStatic } from "frog/serve-static";
 
+import { Box, Heading, Text, VStack, vars, Image } from "@/app/utils/ui";
 
-import { Box, Heading, Text, VStack, vars, Image } from '@/app/utils/ui'
+import { sampleQuestions } from "@/app/utils/sampleQuestions";
 
-import { sampleQuestions } from '@/app/utils/sampleQuestions';
-import backgroundImage from '@/public/bg.png'
+const neynarKey = process.env.NEYNAR_API_KEY ?? "NEYNAR_FROG_FM";
 
-const neynarKey = process.env.NEYNAR_API_KEY ?? 'NEYNAR_FROG_FM';
+interface State {
+  currentIndex: number;
+  responses: Record<number, number>;
+}
 
-const app = new Frog({
-  title: 'Delegate Match',
-  assetsPath: '/',
-  basePath: '/api',
+const app = new Frog<{ State: State }>({
+  title: "Delegate Match",
+  assetsPath: "/",
+  basePath: "/api",
   hub: neynar({ apiKey: neynarKey }),
-  secret: process.env.FROG_SECRET ?? 'FROG_SECRET',
+  secret: process.env.FROG_SECRET ?? "FROG_SECRET",
   verify: true,
-  ui: {vars}
-})
-
+  ui: { vars },
+  initialState: {
+    currentIndex: 0,
+    responses: {},
+  },
+});
 
 const getFrameImage = (title: string) => {
   return (
-    <Box
-    grow
-    alignVertical="center"
-    alignHorizontal='center'
-  >
-    <Image src='/bg.png' objectFit='cover' width='100%' height='100%' />
-    <Box position='absolute' textAlign='center' margin='64'>
-      <Heading size='24' align='center' wrap='balance'>{title}</Heading>
+    <Box grow alignVertical="center" alignHorizontal="center">
+      <Image src="/bg.png" objectFit="cover" width="100%" height="100%" />
+      <Box position="absolute" textAlign="center" margin="64">
+        <Heading size="24" align="center" wrap="balance">
+          {title}
+        </Heading>
+      </Box>
     </Box>
-  </Box>)
-}
+  );
+};
 
-
-app.frame('/', (c) => {
-  const { buttonValue, frameData } = c
-  
-  const fid = frameData?.fid;
-
-  console.log(`fid: ${fid}, value: ${buttonValue}`)
-
+app.frame("/", (c) => {
   return c.res({
-    action: '/0',
-    image: getFrameImage('Start the survey'),
-    intents: [
-      <Button>Start</Button>,
-      // status === 'response' && <Button.Reset>Reset</Button.Reset>,
-    ],
-  })
-})
-
+    action: "/0",
+    image: getFrameImage("Start the survey"),
+    intents: [<Button>Start</Button>],
+  });
+});
 
 sampleQuestions.forEach((question, qid) => {
-  app.frame(`/${question.id}`, (c) => {
-    const { buttonValue, inputText, status, frameData } = c
+  app.frame(`/${qid}`, (c) => {
+    let state;
 
-    console.log({
-      question: question.prompt,
-      choice: question.choices[Number(buttonValue)], buttonValue
-    });
+    const { buttonValue, deriveState } = c;
+
+    if (buttonValue) {
+      state = deriveState((previousState) => {
+        previousState.currentIndex = qid;
+        previousState.responses[qid - 1] = Number(buttonValue);
+      });
+    }
 
     return c.res({
-      action: (qid < sampleQuestions.length-1) ? String(`/${question.id + 1}`) : '/end',
+      action:
+        qid < sampleQuestions.length - 1 ? String(`/${qid + 1}`) : "/loading",
       image: getFrameImage(question.prompt),
-      intents: question.choices.map((choice:string, index:number) => (
+      intents: question.choices.map((choice: string, index: number) => (
         <Button value={String(index)}>{choice}</Button>
-      ))
+      )),
     });
   });
 });
 
-
-app.frame('/end', (c) => {
-  const { buttonValue, inputText, status, frameData } = c
+app.frame("/loading", (c) => {
+  const { buttonValue, deriveState, frameData } = c;
   const fid = frameData?.fid;
-  // console.log({ frameData, fid });
-  console.log(`${fid} , reached the end of questionnaire`, buttonValue)
+
+  let state;
+
+  if (buttonValue) {
+    state = deriveState((previousState) => {
+      previousState.currentIndex = Object.keys(previousState.responses).length;
+      previousState.responses[Object.keys(previousState.responses).length] =
+        Number(buttonValue);
+    });
+  }
+  return c.res({
+    action: "/0",
+    image: getFrameImage("Loading"),
+    intents: [
+      <Button.Link href="https://delegate-match.vercel.app/">
+        See your matches
+      </Button.Link>,
+    ],
+  });
+});
+
+app.frame("/end", (c) => {
+  const { buttonValue, previousState, frameData } = c;
+
+  const fid = frameData?.fid;
+  console.log(
+    { finalState: previousState },
+    `${fid} , reached the end of questionnaire`,
+    buttonValue
+  );
+
   return c.res({
     image: getFrameImage(`#${fid} , your matches await you!`),
     intents: [
-      <Button.Link href="https://delegate-match.vercel.app/">See your matches</Button.Link>,
+      <Button.Link href="https://delegate-match.vercel.app/">
+        See your matches
+      </Button.Link>,
     ],
-  })
-})
+  });
+});
 
+devtools(app, { serveStatic });
 
-
-devtools(app, { serveStatic })
-
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
