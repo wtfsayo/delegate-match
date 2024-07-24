d2d6ab1a7026c47e172e112f3f9377f39d8faabc
memosys <82053242+wtfsayo@users.noreply.github.com>
2024-07-24 20:26:35 +0530

    test!!!

--- app/actions/readAttestations.ts
+++ app/actions/readAttestations.ts
@@ -1,24 +1,26 @@
-import { Address } from "viem";
+import { Address, Hex } from "viem";
 
 import {  optimismGraphQLClient } from "@/app/utils/clients";
 import { GET_ATTESTATIONS } from "@/app/utils/queries";
-import { schemaUID, AttestorAddress } from "@/app/utils/consts";
+// import { schemaUID, AttestorAddress } from "@/app/utils/consts";
 import getFcAddress from "./getFcAddress";
 
 
-const queryVariables = (schemaId: string, attester: Address, recipient: Address) => ({
+const variables = {
   "where": {
     "schemaId": {
-      "equals": schemaId
+      "equals": "0xa62c89f95f67faff1e3ade0db1fca88a09fe53fdc2cc8f41513c22f66f762c79"
     },
     "attester": {
-      "equals": attester
+      "equals": "0xf9467D97A8277D1C4ee4Ad346B1Ca523847019D9"
     },
     "recipient": {
-      "equals": recipient
+      "equals": "0x715D7097C9446B8BDb166557E950cd1Cc5115126"
     }
   }
-})
+}
+
+
 
 
 
@@ -36,7 +38,7 @@ export default async function readAttestations({
 
   const attestations = await optimismGraphQLClient.request(
     GET_ATTESTATIONS,
-    queryVariables(schemaUID, AttestorAddress, recipient)
+    variables
   )
 
   return attestations ?? [];
