b8d340dca37004bc221562f997e7cb649325d03c
memosys <82053242+wtfsayo@users.noreply.github.com>
2024-07-24 18:47:37 +0530

    update consts

--- app/utils/consts.ts
+++ app/utils/consts.ts
@@ -4,19 +4,20 @@ import type { Address, Hex } from "viem";
 import { Wallet, JsonRpcProvider } from "ethers";
 
 
-const relayerPvtKey = process.env.RELAYER_PRIVATE_KEY! as Hex; 
+const relayerPvtKey = process.env.RELAYER_PRIVATE_KEY! as Hex;
 
 
 export const easContractAddress = process.env.EAS_CONTRACT_ADDRESS ?? "0x4200000000000000000000000000000000000021";
 export const schemaUID = process.env.SCHEMA_UID ?? "0xa62c89f95f67faff1e3ade0db1fca88a09fe53fdc2cc8f41513c22f66f762c79";
 export const chain = optimism;
-    // process.env.NODE_ENV === "production" ? optimism : optimismSepolia;
+// process.env.NODE_ENV === "production" ? optimism : optimismSepolia;
 
 // create RPC provider
 export const RPCProvider = new JsonRpcProvider(`https://optimism-mainnet.rpc.grove.city/v1/6ecf8e4b`, {
     name: chain.name,
     chainId: chain.id,
-} ); // TODO: use non public RPC
+}); // TODO: use non public RPC
 
 
 export const AttestationSigner = new Wallet(relayerPvtKey as string, RPCProvider);  // Gas Account
+export const AttestorAddress = AttestationSigner.address as Address;
