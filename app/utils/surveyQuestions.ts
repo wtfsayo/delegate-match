0625ff4e179fc04c61cb4ca79af6d35628dbd68a
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-08-04 18:31:44 +0530

    fix order and expunged qs

--- app/utils/surveyQuestions.ts
+++ app/utils/surveyQuestions.ts
@@ -1,71 +1,56 @@
 export const surveyQuestions = [
   {
-    "id": 1,
+    "id": 0,
     "prompt": "What excites you most about Optimism?",
     "choices": ["Vision", "Tech", "Community"]
   },
   {
-    "id": 2,
+    "id": 1,
     "prompt": "Which is the most crucial differentiator of Optimism?",
     "choices": ["Superchain", "Bi-cameral gov", "Retro funding"]
   },
   {
-    "id": 3,
+    "id": 2,
     "prompt": "What growth area should Optimism prioritize most?",
     "choices": ["Killer Apps", "Superchain growth", "TVL"]
   },
   {
-    "id": 4,
+    "id": 3,
     "prompt": "What should be the highest focus for Optimism tokenomics?",
     "choices": ["Governance", "Engage community", "Onboard developers"]
   },
   {
-    "id": 5,
-    "prompt": "Which of these parameters should be prioritised highest when choosing OP Council and board members?",
-    "choices": ["Member duration", "Relevant skillset", "Career history"]
-  },
-  {
-    "id": 6,
+    "id": 4,
     "prompt": "To what type of projects should we allocate more grants and Retro Funding?",
     "choices": ["Established", "Emerging", "Balanced"]
   },
   {
-    "id": 7,
+    "id": 5,
     "prompt": "Which parameter should receive the highest priority for OP Retro funding?",
     "choices": ["Public Goods", "Mission Alignment", "Potential Impact"]
   },
   {
-    "id": 8,
+    "id": 6,
     "prompt": "What project trait is most important for receiving Optimism funding?",
     "choices": ["VC-backed", "Open Source", "High merit"]
   },
   {
-    "id": 9,
-    "prompt": "How fast should the collective decentralize?",
-    "choices": ["ASAP", "Steady", "On maturity"]
-  },
-  {
-    "id": 10,
+    "id": 7,
     "prompt": "What is the best background and set of skills for a delegate?",
     "choices": ["Technology", "Governance", "Business"]
   },
   {
-    "id": 11,
-    "prompt": "What kind of communication do you expect from your delegate?",
-    "choices": ["Share vote rationale", "Seek feedback", "Not important"]
-  },
-  {
-    "id": 12,
+    "id": 8,
     "prompt": "What excites you the most about the blockchain space?",
     "choices": ["Tech", "Governance", "Public Goods"]
   },
   {
-    "id": 13,
+    "id": 9,
     "prompt": "Which of these areas interest you the most?",
     "choices": ["DeFi", "DAOs/Identity", "Art/Gaming"]
   },
   {
-    "id": 14,
+    "id": 10,
     "prompt": "Given the blockchain trilemma, which do you sacrifice, in the near term, to get the other two?",
     "choices": ["Security", "Scalability", "Decentralization"]
   }
