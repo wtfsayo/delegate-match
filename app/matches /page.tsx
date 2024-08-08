2ad25570e3b66eef5e39fb8c469cf20db669bdcc
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-08-08 15:00:27 +0530

    Update page.tsx

--- app/matches /page.tsx
+++ app/matches /page.tsx
@@ -4,22 +4,20 @@ import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
 
 
-export async function generateMetadata(): Promise<Metadata> {
-  const frameTags = await getFrameMetadata(
-    `${process.env.VERCEL_URL || 'http://localhost:3000'}/api`,
-  )
-  return {
-    other: frameTags,
-  }
-}
+// export async function generateMetadata(): Promise<Metadata> {
+//   const frameTags = await getFrameMetadata(
+//     `${process.env.VERCEL_URL || 'http://localhost:3000'}/api`,
+//   )
+//   return {
+//     other: frameTags,
+//   }
+// }
 
 export default function Home() {
   const fid = useSearchParams().get('fid');
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-      {`Your farcaster ID is ${fid ?? "0"}`}
-      </Suspense>
+      {`Your farcaster ID is ${fid || "0"}`}
     </div>
   )
 }
