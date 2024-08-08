bfc9b79a6e360b19557291f3486aba1a8fbbe744
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-08-08 17:11:37 +0530

    test reading on page

--- app/matches/[fid]/page.tsx
+++ app/matches/[fid]/page.tsx
@@ -2,11 +2,21 @@ import { getFrameMetadata } from 'frog/next'
 import { Metadata } from 'next'
 import { useParams } from 'next/navigation'
 import { Suspense } from 'react'
+import getAttestations from '@/app/actions/attestations'
 
-export default function Page({ params }: { params: { fid: string } }) {
+export default async function Page({ params }: { params: { fid: string } }) {
+  const fid = params.fid;
+  const attestations = await getAttestations(fid);
+  if(!attestations) {
+    return (
+      <main>
+        {`No attestations found for this fid`}
+      </main>
+    )
+  }
   return (
     <div>
-      {`Your farcaster ID is ${params.fid}`}
+      {`Your farcaster ID is ${params.fid} and you have ${attestations.length} attestations`}
     </div>
   )
 }
