e30de965a245c47d440c6dad57c4388e44c1316c
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-08-20 14:14:06 +0530

    test: fix build issues

--- app/page.tsx
+++ app/page.tsx
@@ -16,50 +16,48 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export const LogoColumn = () => {
+const LogoColumn = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <div>
-       <AspectRatio ratio={320 / 224} className="bg-white border-1">
-      <Image
-        src="/dm-vec.png"
-        alt="Delegate Match Mark"
-        fill
-        className="rounded-md object-cover"
-      />
-    </AspectRatio>
-    <AspectRatio ratio={320 / 224} className="bg-white border-1">
-      <Image
-        src="/sun-vec.png" 
-        alt="Optimism Sun like image"
-        fill
-        className="rounded-md object-cover"
-      />
-    </AspectRatio>
+        <AspectRatio ratio={320 / 224} className="bg-white border-1">
+          <Image
+            src="/dm-vec.png"
+            alt="Delegate Match Mark"
+            fill
+            className="rounded-md object-cover"
+          />
+        </AspectRatio>
+        <AspectRatio ratio={320 / 224} className="bg-white border-1">
+          <Image
+            src="/sun-vec.png"
+            alt="Optimism Sun like image"
+            fill
+            className="rounded-md object-cover"
+          />
+        </AspectRatio>
+      </div>
+      <AspectRatio ratio={320 / 224} className="bg-white border-1">
+        <Image
+          src="/logo-dm.png"
+          alt="Delegate Match"
+          fill
+          className="rounded-md object-cover"
+        />
+      </AspectRatio>
+      <AspectRatio ratio={320 / 224} className="bg-white border-1">
+        <Image
+          src="/logo-rg.png"
+          alt="Raidguild"
+          fill
+          className="rounded-md object-cover"
+        />
+      </AspectRatio>
     </div>
-    <AspectRatio ratio={320 / 224} className="bg-white border-1">
-      <Image
-        src="/logo-dm.png"
-        alt="Delegate Match"
-        fill
-        className="rounded-md object-cover"
-      />
-    </AspectRatio>
-    <AspectRatio ratio={320 / 224} className="bg-white border-1">
-      <Image
-        src="/logo-rg.png"
-        alt="Raidguild"
-        fill
-        className="rounded-md object-cover"
-      />
-    </AspectRatio>
-    </div>
-  )
-}
+  );
+};
 
-
-
-export const IntroText = () => {
+const IntroText = () => {
   return (
     <div className="text-left">
       <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
@@ -80,9 +78,6 @@ export const IntroText = () => {
   );
 };
 
-
-
-
 export default function Home() {
-  return <TwoColumnLayout col1={<LogoColumn/>} col2={<IntroText />} />;
+  return <TwoColumnLayout col1={<LogoColumn />} col2={<IntroText />} />;
 }
