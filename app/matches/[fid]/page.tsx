a004a2403daa4bd036bae2f7d767d8e5b5c37208
Sayo <82053242+wtfsayo@users.noreply.github.com>
2024-08-21 17:25:32 +0530

    Update page.tsx

--- app/matches/[fid]/page.tsx
+++ app/matches/[fid]/page.tsx
@@ -3,13 +3,10 @@ import { Metadata } from "next";
 import TwoColumnLayout from "@/components/ui/twoColLayout";
 import getAttestations from "@/app/actions/attestations";
 import rankDelegates from "@/app/actions/matches";
-import {
-  congratsText,
-  homePageIntroText,
-  imagesSingle,
-} from "@/app/utils/consts";
+import { congratsText, imagesSingle } from "@/app/utils/consts";
 import { ImageContainer, ImagesProps } from "@/components/ui/ImageContainer";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { DevBundlerService } from "next/dist/server/lib/dev-bundler-service";
 
 export async function generateMetadata(): Promise<Metadata> {
   const frameTags = await getFrameMetadata(
@@ -36,10 +33,21 @@ export default async function Page({ params }: { params: { fid: string } }) {
   );
 }
 
+
+const MatchColumn: React.FC<{ fid: string }> = async ({ fid }) => {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-4 w-full">
+      <ImageContainer src={imagesSingle[0].src} alt={imagesSingle[0].alt} />
+      <DelegateMatches fid={fid} />
+    </div>
+  );
+};
+
+
 const DelegateMatches: React.FC<{ fid: string }> = async ({ fid }) => {
   const delegateMatches = await rankDelegates(fid!);
   return (
-    <ScrollArea className="h-full w-full rounded-md border p-4">
+    <ScrollArea className="flex flex-col items-center justify-center space-y-4 w-full">
       <ol>
         {delegateMatches.map((delegate) => (
           <li>
@@ -57,45 +65,44 @@ const DelegateMatches: React.FC<{ fid: string }> = async ({ fid }) => {
 
 const CongratsText = () => {
   return (
-    <div className="flex flex-col text-left bg-white h-full p-16 border border-gray-200 rounded-lg gap-6">
-      <span>
-        <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-          {congratsText[0]}
-        </h2>
+    <div className="flex flex-col items-center justify-center space-y-4 w-full">
+      <div className="flex flex-col text-left bg-white h-full p-16 border border-gray-200 rounded-lg gap-6">
+        <span>
+          <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+            {congratsText[0]}
+          </h2>
 
-        {congratsText.slice(1, 3).map((text, index) => (
-          <p key={index} className="leading-7 [&:not(:first-child)]:mt-4">
-            {text}
-          </p>
-        ))}
-      </span>
-      <span>
-        <h2 className="scroll-m-20 pb-2 text-3xl mt-3 font-semibold tracking-tight mt-3">
-          {congratsText[3]}
-        </h2>
+          {congratsText.slice(1, 3).map((text, index) => (
+            <p key={index} className="leading-7 [&:not(:first-child)]:mt-4">
+              {text}
+            </p>
+          ))}
+        </span>
+        <span>
+          <h2 className="scroll-m-20 pb-2 text-3xl mt-3 font-semibold tracking-tight mt-3">
+            {congratsText[3]}
+          </h2>
 
-        {congratsText.slice(4, 6).map((text, index) => (
-          <p key={index} className="leading-7 [&:not(:first-child)]:mt-4">
-            • {text}
-          </p>
-        ))}
-      </span>
+          {congratsText.slice(4, 6).map((text, index) => (
+            <p key={index} className="leading-7 [&:not(:first-child)]:mt-4">
+              • {text}
+            </p>
+          ))}
+        </span>
 
-      <span>
-        <h2 className="scroll-m-20 pb-2 text-3xl mt-3 font-semibold tracking-tight mt-3">
-          {congratsText[6]}
-        </h2>
+        <span>
+          <h2 className="scroll-m-20 pb-2 text-3xl mt-3 font-semibold tracking-tight mt-3">
+            {congratsText[6]}
+          </h2>
 
-        {congratsText.slice(7, 10).map((text, index) => (
-          <p key={index} className="leading-7 [&:not(:first-child)]:mt-4">
-            {index + 1}. {text}
-          </p>
-        ))}
-      </span>
-      {/* <ImageContainer
-          src={imagesSingle[0].src}
-          alt={imagesSingle[0].alt}
-        /> */}
+          {congratsText.slice(7, 10).map((text, index) => (
+            <p key={index} className="leading-7 [&:not(:first-child)]:mt-4">
+              {index + 1}. {text}
+            </p>
+          ))}
+        </span>
+      </div>
+      <ImageContainer src={imagesSingle[1].src} alt={imagesSingle[1].alt} /> 
     </div>
   );
 };
