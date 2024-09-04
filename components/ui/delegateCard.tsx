5a6f4dcdffcdd2913f2bb7894e12d58af54b4f57
suede0619 <suede0619@gmail.com>
2024-09-04 15:56:47 -0600

    target blank for delegate button

--- components/ui/delegateCard.tsx
+++ components/ui/delegateCard.tsx
@@ -94,7 +94,7 @@ export const DelegateCard = async ({ delegate }: { delegate: RankedDelegate }) =
                     </Avatar>
                     <h2 className="font-semibold ">{delegate.matchPercentage}% Match</h2>
                 </span>
-                <Link href={`https://vote.optimism.io/delegates/${delegate.delegateID}`} passHref>
+                <Link href={`https://vote.optimism.io/delegates/${delegate.delegateID}`} target={`_blank`} passHref>
                     <div className="bg-white border border-gray-200 rounded-md px-4 py-2 hover:bg-slate-100 mt-5 sm:mt-0">
                         View Delegate
                     </div>
